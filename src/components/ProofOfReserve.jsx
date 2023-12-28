dd04d2b9717b73c23b965410c24e192f63bedaef
isnemesis <gagandt.dev@gmail.com>
2023-12-29 00:56:03 +0900

    chore: fixed proof of reserves

--- src/components/ProofOfReserve.jsx
+++ src/components/ProofOfReserve.jsx
@@ -1,51 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import TOKEN_ABI from '../utils/tokenAbi';
-import Text from './Text';
+import React, { useEffect, useState } from "react";
+import TOKEN_ABI from "../utils/tokenAbi";
+import Text from "./Text";
 
-const ProofOfReserve = ({ token, ethWeb3, avaxWeb3, ethContractHandler, avaxContractHandler }) => {
+const ProofOfReserve = ({
+  token,
+  ethWeb3,
+  avaxWeb3,
+  ethContractHandler,
+  avaxContractHandler,
+}) => {
   const [ercBalance, setErcBalance] = useState(0);
   const [arcBalance, setArcBalance] = useState(0);
 
   useEffect(() => {
-    ethContractHandler.methods.tokenContracts(token.token).call((err, result) => {
-      if (err) {
-        console.error('Error: ', err);
-        // handle the error here
-      }
+    ethContractHandler.methods
+      .tokenContracts(token.token)
+      .call()
+      .then((result) => {
+        if (result !== "0x0000000000000000000000000000000000000000") {
+          const tokenContractHandler = new ethWeb3.eth.Contract(
+            TOKEN_ABI,
+            result,
+          );
+          console.log({ call: 1, result });
 
-      if (result !== '0x0000000000000000000000000000000000000000') {
-        const tokenContractHandler = new ethWeb3.eth.Contract(TOKEN_ABI, result);
-        tokenContractHandler.methods.totalSupply().call((err, result) => {
-          if (result) {
-            const supply = ethWeb3.utils.fromWei(result, 'ether');
-            setErcBalance(supply);
-          }
-        });
-      }
-    });
+          tokenContractHandler.methods
+            .totalSupply()
+            .call()
+            .then((result) => {
+              if (result) {
+                const supply = ethWeb3.utils.fromWei(result, "ether");
+                console.log({ call: 1, result });
+                setErcBalance(supply);
+              }
+            });
+        }
+      });
 
-    avaxContractHandler.methods.tokenContracts(token.token).call((err, result) => {
-      if (err) {
-        console.error('Error: ', err);
-        // handle the error here
-      }
+    avaxContractHandler.methods
+      .tokenContracts(token.token)
+      .call()
+      .then((result) => {
+        if (result !== "0x0000000000000000000000000000000000000000") {
+          const tokenContractHandler = new avaxWeb3.eth.Contract(
+            TOKEN_ABI,
+            result,
+          );
 
-      if (result !== '0x0000000000000000000000000000000000000000') {
-        const tokenContractHandler = new avaxWeb3.eth.Contract(TOKEN_ABI, result);
-        tokenContractHandler.methods.totalSupply().call((err, result) => {
-          if (result) {
-            const supply = avaxWeb3.utils.fromWei(result, 'ether');
-            setArcBalance(supply);
-          }
-        });
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+          tokenContractHandler.methods
+            .totalSupply()
+            .call()
+            .then((result) => {
+              if (result) {
+                const supply = avaxWeb3.utils.fromWei(result, "ether");
+                setArcBalance(supply);
+                console.log(supply, "supply");
+              }
+            });
+        }
+      });
   }, []);
 
   return (
     <div className="dashboard-stat-card">
-      <Text className="text-[32px] dashboard-stat-card--title font-bold" size="txtSyneBold40">
+      <Text
+        className="text-[32px] dashboard-stat-card--title font-bold"
+        size="txtSyneBold40"
+      >
         {token?.token?.toUpperCase()}
       </Text>
       <p className="text-[#C2C2C2] my-3">Token Supply</p>
@@ -55,10 +76,10 @@ const ProofOfReserve = ({ token, ethWeb3, avaxWeb3, ethContractHandler, avaxCont
       </div>
       <div className="flex justify-content-between mb-4">
         <p>ERC20</p>
-        <p>{parseInt(token?.balance, 10)?.toLocaleString()}</p>
+        <p>{parseInt(ercBalance, 10)?.toLocaleString()}</p>
       </div>
       <div className="flex justify-content-between mb-4">
-        <p>Existing cross-chain amount</p>
+        <p>ARC20</p>
         <p> {parseInt(arcBalance, 10)?.toLocaleString()}</p>
       </div>
     </div>
