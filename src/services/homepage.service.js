8424cb247ee928993fcbc877adf8baec6f92c757
isnemesis <gagandt.dev@gmail.com>
2023-12-30 04:28:34 +0900

    fix: inscribe api

--- src/services/homepage.service.js
+++ src/services/homepage.service.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const tickerList = async ({ limit, page }) => {
   try {
     const data = await axios.get(
-      `https://api.ordbridge.io/bapi/bridge/tokens?limit=${limit}&offset=${page}`,
+      `https://api.ordbridge.io/bapi/bridge/tokens?limit=${limit}&offset=${page}`
     );
     return data?.data;
   } catch (error) {
@@ -14,7 +14,7 @@ export const updateAddress = async (payload) => {
   try {
     const data = await axios.post(
       `https://api.ordbridge.io/bapi/bridge/update_user_details`,
-      payload,
+      payload
     );
     return data;
   } catch (error) {
@@ -23,10 +23,7 @@ export const updateAddress = async (payload) => {
 };
 export const initiateBridge = async ({ body, session_key }) => {
   try {
-    const data = await axios.post(
-      `https://api.ordbridge.io/bapi/bridge/init_payment`,
-      body,
-    );
+    const data = await axios.post(`https://api.ordbridge.io/bapi/bridge/init_payment`, body);
     // const data = await axios.post(`https://api.ordbridge.io/bapi/bridge/init_payment`, body, {
     //   headers: {
     //     xid: session_key
@@ -37,44 +34,33 @@ export const initiateBridge = async ({ body, session_key }) => {
     throw error;
   }
 };
-export const pendingEntryService = async ({
-  session_key,
-  unisatAddress,
-  metaMaskAddress,
-}) => {
+export const pendingEntryService = async ({ session_key, unisatAddress, metaMaskAddress }) => {
   try {
-    const data = await axios.post(
-      `https://api.ordbridge.io/bapi/bridge/pending_entries`,
-      {
-        unisat_address: unisatAddress,
-        metamask_address: metaMaskAddress,
-      },
-    );
+    const data = await axios.post(`https://api.ordbridge.io/bapi/bridge/pending_entries`, {
+      unisat_address: unisatAddress,
+      metamask_address: metaMaskAddress
+    });
     return data?.data;
   } catch (error) {
     throw error;
   }
 };
-export const inscribeService = async ({
-  res,
-  metaMaskAddress,
-  unisatAddress,
-}) => {
+export const inscribeService = async ({ res, metaMaskAddress, unisatAddress }) => {
   try {
-    const data = await axios.post(
-      `https://api.ordbridge.io/bapi/bridge/inscribe`,
-      {
-        inscribe_json: {
-          amt: res?.inscribe?.amt,
-          ethchain: res?.inscribe?.ethchain,
-          op: res?.inscribe?.op,
-          p: res?.inscribe?.p,
-          tick: res?.inscribe?.tick,
-        },
-        unisat_address: unisatAddress,
-        metamask_address: metaMaskAddress,
+    const data = await axios.post(`https://api.ordbridge.io/bapi/bridge/inscribe`, {
+      inscribe_json: {
+        amt: res?.inscribe?.amt,
+        ethchain: res?.inscribe?.ethchain,
+        avaxchain: res?.inscribe?.avaxchain,
+        arbichain: res?.inscribe?.arbichain,
+        basechain: res?.inscribe?.basechain,
+        op: res?.inscribe?.op,
+        p: res?.inscribe?.p,
+        tick: res?.inscribe?.tick
       },
-    );
+      unisat_address: unisatAddress,
+      metamask_address: metaMaskAddress
+    });
     return data?.data?.data;
   } catch (error) {
     throw error;
@@ -82,12 +68,9 @@ export const inscribeService = async ({
 };
 export const transferService = async ({ session_key, InscriptionId }) => {
   try {
-    const data = await axios.post(
-      `https://api.ordbridge.io/bapi/bridge/transfer`,
-      {
-        inscription_id: InscriptionId,
-      },
-    );
+    const data = await axios.post(`https://api.ordbridge.io/bapi/bridge/transfer`, {
+      inscription_id: InscriptionId
+    });
     return data?.data?.data;
   } catch (error) {
     throw error;
@@ -95,12 +78,9 @@ export const transferService = async ({ session_key, InscriptionId }) => {
 };
 export const deleteEntry = async ({ session_key, reference_id }) => {
   try {
-    const data = await axios.delete(
-      `https://api.ordbridge.io/bapi/bridge/delete_entries`,
-      {
-        reference_id: reference_id,
-      },
-    );
+    const data = await axios.delete(`https://api.ordbridge.io/bapi/bridge/delete_entries`, {
+      reference_id: reference_id
+    });
     return data?.data;
   } catch (error) {
     throw error;
