b6a04825b67b57bf5adbf7ff7d8cb8a3fa33af4a
isnemesis <gagandt.dev@gmail.com>
2024-01-03 23:53:23 +0900

    fix: mobile

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -26,9 +26,14 @@ export const Banner = () => {
       <Confetti height={60} opacity={0.5} gravity={0.02} numberOfPieces={150} />
       <div className="bg-[#271F3A] h-[60px] flex items-center justify-center text-syne text-white">
         <Text
-          className="text-white-A700 text-base cursor-pointer ml-6 mr-2 !mb-0 block sm:hidden text-xl"
+          className="sm:hidden text-white-A700 text-base cursor-pointer ml-6 mr-2 !mb-0 block sm:text-md text-xl"
           size="txtSyneBold20">
           We are live on{'  '}
+        </Text>
+        <Text
+          className="sm:block hidden text-white-A700 text-base cursor-pointer ml-6 mr-2 !mb-0 block text-xl"
+          size="txtSyneBold20">
+          Live on{'  '}
         </Text>
         <img src={SolanaIcon} alt="Solana" />
 
