6ce3a1f542f28bc96ef258b72a87962a2e83a5e0
ArjunUdyogYantra <arjun.magun@udyogyantra.com>
2024-01-10 02:16:38 +0530

    Fix: Build Fixes

--- src/components/ProcessSteps/Step4.jsx
+++ src/components/ProcessSteps/Step4.jsx
@@ -7,12 +7,12 @@ export const Step4 = ({
   setClaimButton,
   setClaimStatus,
   swap,
-  fromChain,
+  fromChain
 }) => {
   return (
     <>
       <div className="first_container">
-        <div className="custom_popup rounded-3xl background_popup text-center">
+        <div className="rounded-3xl background_popup text-center">
           <header className="popup_header">
             <div className="text-center">
               <div className="swap_header font-syne">
@@ -26,25 +26,20 @@ export const Step4 = ({
                 <>The transaction failed as user rejected</>
               ) : (
                 <>
-                  Your tokens have been received{" "}
-                  <span className="fw-bold">successfully</span>
+                  Your tokens have been received <span className="fw-bold">successfully</span>
                 </>
               )}
             </div>
           </section>
           {claimStatus === "success" && !swap && fromChain.tag !== "SOL" && (
             <section className="form_data_section3 p-2 font-syne">
-              <div className="fw-semibold">
-                You can check pending entries at anytime.
-              </div>
+              <div className="fw-semibold">You can check pending entries at anytime.</div>
             </section>
           )}
           {!swap && claimStatus === "success" && fromChain.tag !== "SOL" && (
             <section className="form_data_section p-2 font-syne">
               <div className="fw-normal flex align-items-center">
-                <>
-                  Please Wait 30 minutes for the bridging process to complete.
-                </>
+                <>Please Wait 30 minutes for the bridging process to complete.</>
               </div>
             </section>
           )}
@@ -56,8 +51,7 @@ export const Step4 = ({
                 setClaimButton(false);
                 setClaimStatus("success");
                 setStep(0);
-              }}
-            >
+              }}>
               Initiate Another Bridge
             </button>
           </footer>
