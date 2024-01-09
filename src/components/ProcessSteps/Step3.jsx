80a5e739f8432b7d40ac524aa2f4a39f19e22b70
ArjunUdyogYantra <arjun.magun@udyogyantra.com>
2024-01-09 22:51:08 +0530

    Fix: UI Fixes

--- src/components/ProcessSteps/Step3.jsx
+++ src/components/ProcessSteps/Step3.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "../../styles/FormStep.css";
+import React from 'react';
+import '../../styles/FormStep.css';
 export const Step3 = ({ setStep, handleBack }) => {
   return (
     <>
       <div className="first_container">
-        <div className="bg-[#111331] border border-black rounded-xl py-32">
+        <div className="rounded-xl flex flex-col justify-center items-center swap-popup-container">
           {/* <img src="BackIcon.png" className="back_icon" onClick={handleBack} /> */}
           <header className="popup_header">
             <div className="text-center">
-              <div className="swap_header">Transaction in progress</div>
+              <div className="swap_header font-syne">Transaction in progress</div>
             </div>
           </header>
-          <section className="form_data_section3">
+          <section className="form_data_section3 font-syne">
             <div>
               Just a sec.. <br />
               We are verifying your transaction
             </div>
           </section>
           <footer className="min-w-full flex items-center">
-            <img src="infinity.gif" className="infinity" alt="" />
+            <img src="infinity.gif" className="infinity w-[5rem]" alt="" />
           </footer>
         </div>
       </div>
