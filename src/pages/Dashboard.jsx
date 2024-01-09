e9e8da48a6168c0a715a58e046eca04092ea6d6c
himanshuguptaAntino <himanshu.gupta@antino.com>
2024-01-09 11:51:55 +0530

    add token brge constarint and replace price with price*230 in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -57,7 +57,7 @@ const Dashboard = ({ appChains }) => {
         <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 gap-5 text-white">
           <DashboardStatCard
             title="Price"
-            value={`$${statsData?.lastPrice ? statsData.lastPrice.substring(0, 7) : 'Loading...'}`}
+            value={`$${statsData?.lastPrice ? `${230*Number(statsData.lastPrice.substring(0, 7))}M` : 'Loading...'}`}
           />
           <DashboardStatCard title="Total Volume" value={'$16.7M'} />
         </div>
